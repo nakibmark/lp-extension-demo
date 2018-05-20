f8dbe4c5d1049a90de84816f3fa65ac995e6a7a1
Mark Nakib <mnakib@protonmail.com>
2018-05-20 01:42:00 -0400

    Add functionality to clicking element

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,6 @@
-console.log("Content script loaded successfully!");
+let nodes = document.getElementsByTagName("h1");
+for (let node of nodes) {
+    node.addEventListener("click", event => {
+        window.alert("You clicked on the H1!")
+    });
+}

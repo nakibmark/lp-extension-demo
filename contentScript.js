da9c7ec02af59e46b35aa0fc98f8078cabe6e880
Mark Nakib <mnakib@protonmail.com>
2018-05-21 20:47:20 -0400

    Modify to intercept link clicks

--- contentScript.js
+++ contentScript.js
@@ -1,6 +1,7 @@
-let nodes = document.getElementsByTagName("h1");
+let nodes = document.getElementsByTagName("a");
 for (let node of nodes) {
     node.addEventListener("click", event => {
-        window.alert("You clicked on the H1!")
+        event.preventDefault();
+        window.alert("You clicked on " + node.textContent + "!");
     });
 }

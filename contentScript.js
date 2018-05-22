0ceac8ce86e274dea76492720b10824ed85bf99e
Mark Nakib <mnakib@protonmail.com>
2018-05-21 22:22:17 -0400

    Refactor content script
    Content script was refactored to inject the modal HTML and then run the
    rest of the JS following the successful loading of that HTML. JS was
    placed into its own function for tighter variable scoping.

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,27 @@
-let nodes = document.getElementsByTagName("a");
-for (let node of nodes) {
-    node.addEventListener("click", event => {
-        event.preventDefault();
-        window.alert("You clicked on " + node.textContent + "!");
-    });
+fetch(chrome.extension.getURL("/modal.html"))
+    .then(response => response.text())
+    .then(data => {
+        document.body.innerHTML += data;
+        modalFunction();
+    })
+    .catch(err => console.error("Extension error loading modal html: " + err))
+
+function modalFunction(){
+    let links = document.getElementsByTagName("a");
+    let modal = document.getElementById("extension-modal");
+    let close = document.getElementById("close-modal");
+
+    for (let link of links) {
+        link.addEventListener("click", event => {
+            event.preventDefault();
+            modal.style.display = "block";
+        });
+    }
+
+    close.onclick = () => modal.style.display = "none";
+    window.onclick = event => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 }
